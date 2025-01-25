42bd9bdf1b3a950630b407efcc7e8b97a5ac4292
WillSeguin <will.seguin2016@gmail.com>
2025-01-24 17:36:34 -0800

    Updated Cancoder init

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -1,21 +1,21 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Rotations;
 import static frc.robot.Constants.*;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.MagnetSensorConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 
-//import com.ctre.motorcontrol.can.FalconSRX;
-
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-
-//TalonFX myMotor = new TalonFX(10); - motor
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
 
 /*In entire lift:
 2 limit switches, 
@@ -28,43 +28,51 @@ import com.ctre.phoenix6.hardware.TalonFX;
 public class Lift extends SubsystemBase {
     private TalonFX liftMotor;
     private TalonFXConfiguration liftMotorConfig;
+
     private CANcoder spinLiftCANCoder;
+    private CANcoderConfiguration canCoderConfig;
+
     private DigitalInput toplimitSwitch = new DigitalInput(0);
     private DigitalInput bottomlimitSwitch = new DigitalInput(1);
+
     private double startingAngle;
+
     private PositionVoltage m_request;
-    private MagnetSensorConfigs canCoderConfig = new MagnetSensorConfigs();
     // in init function, set slot 0 gains
 
     public Lift() {
         // CANCoder
         spinLiftCANCoder = new CANcoder(KLiftCANCoderID);
-
-        double offsetToRotations = 0;
-
-
-        // canCoderConfig.AbsoluteSensorDiscontinuityPoint = AbsoluteSensorDiscontinuityPoint;
-        // canCoderConfig.SensorDirection = SensorDirectionValue.Clockwise_Positive;
-        // canCoderConfig.MagnetOffset = offsetToRotations;
+        canCoderConfig = new CANcoderConfiguration();
+        // canCoderConfig.MagnetSensor.withAbsoluteSensorDiscontinuityPoint(Rotations.of(0.5));
+        canCoderConfig.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
         spinLiftCANCoder.getConfigurator().apply(canCoderConfig);
-        var slot0Configs = new Slot0Configs();
-        liftMotor.getConfigurator().apply(liftMotorConfig);
+
+        // Motor
         liftMotor = new TalonFX(KSpinMotorID);
+
+        liftMotorConfig = new TalonFXConfiguration();
+        liftMotorConfig.Feedback.FeedbackRemoteSensorID = spinLiftCANCoder.getDeviceID();
+        liftMotorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+        liftMotorConfig.Feedback.SensorToMechanismRatio = 1.0;
+        liftMotorConfig.Feedback.RotorToSensorRatio = 46.67;
+        
+        liftMotor.getConfigurator().apply(liftMotorConfig);
+
+        //pid config
+        var slot0Configs = new Slot0Configs();
         slot0Configs.kP = 0; // An error of 1 rotation results in 2.4 V output
         slot0Configs.kI = 0; // no output for integrated error
         slot0Configs.kD = 0; // A velocity of 1 rps results in 0.1 V output
 
         liftMotor.getConfigurator().apply(slot0Configs);
+
         m_request = new PositionVoltage(0).withSlot(0);
 
+        startingAngle = 0;
     }
 
-    // Limit Switches
-    @Override
-    public void periodic() {
-
-    }
-
+    
     public void setMotorSpeed(double speed) {
         if (speed > 0) {
             if (toplimitSwitch.get()) {
@@ -80,19 +88,22 @@ public class Lift extends SubsystemBase {
             }
         }
     }
-
+    
     // PID
-    public void SetLiftToPoint(double setpoint) {
-        // create a position closed-loop request, voltage output, slot 0 configs
-
-        // set position to 10 rotations (degrees/360!)
+    public void SetLiftToPos(double setpoint) {
+        //In rotations (degrees/360)
         liftMotor.setControl(m_request.withPosition(setpoint));
     }
-
+    
     public double getLiftCANCoder() {
         double angle = spinLiftCANCoder.getPosition().getValueAsDouble() * 360 + startingAngle;
-        // startingAngle = 0 - spinLiftCANCoder.getPosition().getValueAsDouble()*360;
         return angle;
+        
+    }
+
+    // Limit Switches
+    @Override
+    public void periodic() {
 
     }
 }
